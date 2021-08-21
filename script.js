bb47aa30f034e7ef1b5b5ef36356dc8f322e4401
Esteban Palacio <estipalacio@gmail.com>
2021-08-21 14:34:20 -0400

    added controls for max library size

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
-const prevBtn = document.getElementById('prev')
+const prevBtn = document.getElementById('previous')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Good night, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (remix)',
+        artist: 'Jacinto Design'
+    }
+]
 
 // Check if playing
 let isPlaying = false
@@ -24,3 +51,41 @@ const pauseSong = () => {
 
 // Play or Pause - Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// Update DOM
+const loadSong = (song) => {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+// Current song
+let songIndex = 0
+
+// Previous song
+const prevSong = () => {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// Next song
+const nextSong = () => {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// On Load - Select first song
+loadSong(songs[songIndex])
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
